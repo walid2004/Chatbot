0a5f57bdc2b8ec0e7f2314efd38f0399f5b663b6
waled <lodaragab@gmail.com>
2025-04-13 00:46:58 +0200

    1.21

--- app/components/chat.js
+++ app/components/chat.js
@@ -1,29 +1,28 @@
 'use clien'
 import * as motion from 'motion/react-client'
+import '../../styles/chat.css'
 import { IoIosSend } from "react-icons/io";
 
 const bg2 = () => {
   return (
-    <div >
         <form>
             <input type = 'text' autoFocus >
               </input>
               <div className='sendingbutton'>
               <motion.div animate={{
                 scale:[1.2,1,1.2,1.2],
-                rotate:[0,-40,0,-30,0],
+                rotate:[0,-30,0,-30,0],
                 ease: 'easeInOut',
               }}
               transition={{
                 ase: 'easeInOut',
                 repeat: Infinity,
-                duration: 3, times: [1, 1, 1]
+                duration: 10, times: [1, 1, 1]
               }}>
                 <IoIosSend size={30} color='white' style={{padding:'5'}}/>
               </motion.div>
               </div>
         </form>
-    </div>
   )
 }
 
