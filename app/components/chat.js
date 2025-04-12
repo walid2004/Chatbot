3ecc57c0c0b4ad0aea42151b854d663424658518
waled <lodaragab@gmail.com>
2025-04-13 00:55:24 +0200

    1.22

--- app/components/chat.js
+++ app/components/chat.js
@@ -6,9 +6,8 @@ import { IoIosSend } from "react-icons/io";
 const bg2 = () => {
   return (
         <form>
-            <input type = 'text' autoFocus >
-              </input>
-              <div className='sendingbutton'>
+            <input className='bar' type = 'text' autoFocus ></input>
+              <div className='phew'>
               <motion.div animate={{
                 scale:[1.2,1,1.2,1.2],
                 rotate:[0,-30,0,-30,0],
